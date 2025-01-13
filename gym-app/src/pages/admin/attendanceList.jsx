a8e20c48ab7ca236d8dd91d5bd068e53880909f2
오똥땅 <ajtwodtmdnjs@gmail.com>
2025-01-13 13:17:40 +0900

    pages 추가

---
+++ gym-app/src/pages/admin/attendanceList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const attendanceList = () => {
+  return (
+    <div>출석내역화면</div>
+  )
+}
+
+export default attendanceList
---
+++ gym-app/src/pages/user/check.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const check = () => {
+  return (
+    <div>출석체크 확인 화면</div>
+  )
+}
+
+export default check
