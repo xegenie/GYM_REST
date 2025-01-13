40cd0baadd95904d7a85ee4f65ebebb5d181a92d
xegenie <dltpwlswkdf@naver.com>
2025-01-13 12:02:45 +0900

    폴더파일

---
+++ gym-app/src/pages/admin/SaleList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SaleList = () => {
+  return (
+    <div>SaleList</div>
+  )
+}
+
+export default SaleList
---
+++ gym-app/src/pages/user/NormalTicket.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NormalTicket = () => {
+  return (
+    <div>NormalTicket</div>
+  )
+}
+
+export default NormalTicket
