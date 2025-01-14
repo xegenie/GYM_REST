92f9362b1b256476107db87c5e0a1f32f04b2d6a
오똥땅 <ajtwodtmdnjs@gmail.com>
2025-01-14 17:19:52 +0900

    ㅎㅇ

---
+++ gym-app/src/pages/user/QrCode/QrCode.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './qrCode.css'; // CSS 파일 임포트
+
+const QrCode = () => {
+  return (
+    <div>
+      <h2 className='zz'>QR 코드 화면</h2>
+      <p>여기에서 QR 코드를 생성할 수 있습니다.</p>
+    </div>
+  );
+};
+
+export default QrCode;
--- gym-app/src/pages/user/ranking.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Ranking = () => {
-  return (
-    <div>랭킹화면</div>
-  )
-}
-
-export default Ranking
