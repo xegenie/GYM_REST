3dc21297336f3b7d8963b953a26824ee31c04efe
LimeYun <jstar3183@naver.com>
2025-01-14 21:29:57 +0900

    예약목록 진행중

--- gym-app/src/apis/reservation.js
+++ gym-app/src/apis/reservation.js
@@ -1,8 +1,3 @@
 import api from './api'
 
-export const list = (keyword, option, page) => {
-    const orderCode = option ?.orderCode || 0;
-    const code = option ?.code || 0;
-
-    return api.get(`/admin/reservation/list?keyword=${keyword}&orderCode=${orderCode}&code=${code}&page=${page}`);
-};
+export const list = (keyword, option, page) => api.get(`/admin/reservation/list?keyword=${keyword}&option=${option}&page=${page}`)
