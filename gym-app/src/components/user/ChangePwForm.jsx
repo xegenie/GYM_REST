abcb418d38209215c15aa1e4392344ea87b2cd47
t326598 <t326598@naver.com>
2025-01-22 14:40:16 +0900

    sdfdfs

--- gym-app/src/components/user/ChangePwForm.jsx
+++ gym-app/src/components/user/ChangePwForm.jsx
@@ -1,82 +1,85 @@
-import React, { useState } from 'react';
-import * as Swal from '../../apis/alert'
+import React from 'react';
+import * as Swal from '../../apis/alert';
+import styles from '../../components/user/css/ChangePw.module.css'; // 모듈화된 CSS import
+import logo from '../../assets/imges/logo2.png';
 
-
-const ChangePwForm = ({changePw}) => {
-
-  const changePws = (e) => {
+const NewPwForm = ({ changePw }) => {
+  const newPassword = (e) => {
     e.preventDefault();
     const form = e.target;
     const password = form.password.value;
-    const newPassword = form.newPassword.value;
     const passwordCheck = form.passwordCheck.value;
+    const userdata = sessionStorage.getItem('userData');
+    const code = JSON.parse(userdata);
 
-      if (newPassword !== passwordCheck) {
-          Swal.alert('비밀번호가 일치하지 않습니다.', '비밀번호를 확인해주세요', 'error');
-          
-          const passwordField = document.getElementById("password");
-          passwordField.focus();
-          
-          
-          return;
-        }
+    if (password !== passwordCheck) {
+      Swal.alert('비밀번호가 일치하지 않습니다.', '비밀번호를 확인해주세요', 'error');
 
-        changePw({ password, newPassword });
-}
+      const passwordField = document.getElementById('password');
+      passwordField.focus();
+      return;
+    }
+
+    changePw({ password, code });
+  };
 
   return (
-    <div style={{ marginTop: 50, marginBottom: 50 }} className="container1 col-12 col-lg-4" id="contain-box">
+    <body className={`${styles.body}`}>
+
+    
+    <div className={`${styles.container1}`} id="contain-box">
       <div className="px-4 py-5 mt-5 text-center">
-        <img className="FITlogo" src="/img/logo2.png" alt=""/>
-        <h1 className="joins">비밀번호 변경</h1>
+           <img className={styles.FITlogo} src={logo} alt="" />
+        <h1 className={styles.h1}>비밀번호 변경</h1>
       </div>
 
-      <form id="form" onSubmit={(e) => changePws(e)}>
-        <div className="centerdhkwnj">
+      <form id="form" onSubmit={(e) => newPassword(e)}>
+        <div className={styles.centerdhkwnj}>
+          <div className={styles.inputtags}>
           <div className="input-group my-2">
-            <label>현재 비밀번호</label>
+            <div className={styles.label2}>
+            <label className={styles.label}>새 비밀번호</label>
+            </div>
+            <div className={styles.formCenter}>
             <input
               type="password"
-              id='password'
-              name='password'
-              autoComplete='password'
-              className="form-control"
-              placeholder="현재 비밀번호를 입력해주세요."
-            />
-          </div>
-
-          <div style={{ marginTop: 20 }} className="input-group my-2">
-            <label>새 비밀번호</label>
-            <input
-              type="password"
-              className="form-control"
-              id='newPassword'
-              name='newPassword'
-              autoComplete='newPassword'
+              className={styles.formControl}
+              id="password"
+              name="password"
+              autoComplete="password"
               placeholder="새 비밀번호를 입력해주세요."
             />
           </div>
-
-          <div style={{ marginTop: 20 }} className="input-group my-2">
-            <label>새 비밀번호 확인</label>
-            <input
-              type="password"
-              id='passwordCheck'
-              name='passwordCheck'
-              autoComplete='passwordCheck'
-              className="form-control"
-              placeholder="새 비밀번호를 입력해주세요."
-            />
           </div>
 
-          <div style={{ marginTop: 10 }}>
-            <button className="btn-join" type="submit">비밀번호 변경</button>
+          <div className="input-group my-2" >
+          <div>
+            <label className={styles.label}>새 비밀번호 확인</label>
+             </div>
+            <div className={styles.formCenter}>
+            <input
+              type="password"
+              id="passwordCheck"
+              name="passwordCheck"
+              autoComplete="passwordCheck"
+              className={styles.formControl}
+              placeholder="새 비밀번호를 입력해주세요."
+            />
+            </div>
+          </div>
+          </div>
+
+          <div style={{ marginTop: 10, textAlign: 'center' }}>
+            <button className={styles.btnJoin} type="submit">
+              비밀번호 변경
+            </button>
           </div>
         </div>
       </form>
+    
     </div>
+    </body>
   );
 };
 
-
-export default ChangePwForm
+export default NewPwForm;
